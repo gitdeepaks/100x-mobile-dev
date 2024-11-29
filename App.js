6e1bd30224549a72b83538c9d16e4118a1634077
gitdeepaks <deepaksankhyan92@outlook.com>
2024-11-29 20:13:11 +0530

    conditional rendaring

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {
   ScrollView,
   Button,
   Linking,
+  ActivityIndicator,
 } from "react-native";
 import { FontAwesome6 } from "@expo/vector-icons";
 import {
@@ -37,19 +38,43 @@ export default function App() {
   const renderIcons = () => {
     return (
       <View style={{ flexDirection: "row", gap: 15, marginVertical: 10 }}>
-        <FontAwesome6 name="github" size={25} color="#252525" />
-        <FontAwesome6 name="x-twitter" size={24} color="#252525" />
+        {links.github && (
+          <FontAwesome6 name="github" size={25} color="#252525" />
+        )}
+        {links.x && <FontAwesome6 name="x-twitter" size={24} color="#252525" />}
         <FontAwesome6 name="instagram" size={24} color="#252525" />
         <FontAwesome6 name="linkedin" size={24} color="#252525" />
         <FontAwesome6 name="facebook" size={24} color="#252525" />
         <FontAwesome6 name="youtube" size={24} color="#252525" />
         <FontAwesome6 name="whatsapp" size={24} color="#252525" />
         <FontAwesome6 name="telegram" size={24} color="#252525" />
-        <FontAwesome6 name="at" size={24} color="#252525" />
+        {links.email && <FontAwesome6 name="at" size={24} color="#252525" />}
       </View>
     );
   };
 
+  // const isLoading = true;
+  // const isError = true;
+
+  //   const likes = 10;
+
+  //   // if (isLoading)
+  //   //   return (
+  //   //     <ActivityIndicator size="large" color="#252525" style={{ flex: 1 }} />
+  //   //   );
+
+  //   return (
+  //     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+  //       <Text>Post has {likes || "no"} likes</Text>
+  //     </View>
+  //   );
+  // }
+
+  const links = {
+    github: "gitdeepaks",
+    email: "sankhyan.deepak1@gmail.com",
+    x: "sankhyandeepak92",
+  };
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={["bottom"]}>
