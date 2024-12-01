0d2e6c477f5a0a0a4f86ae6b070aabbd4ed2f14a
gitdeepaks <deepaksankhyan92@outlook.com>
2024-12-01 15:26:21 +0530

    completed business card

--- cards/ProjectCard.jsx
+++ cards/ProjectCard.jsx
@@ -12,22 +12,24 @@ import {
 const ProjectCard = ({ name, image }) => {
   return (
     <View>
-      <Image
-        source={image}
-        style={{ height: 150, aspectRatio: 16 / 9, borderRadius: 10 }}
-      />
-      <Text
-        style={{
-          fontWeight: "bold",
-          fontSize: 20,
-          color: "dimgray",
-          marginTop: 10,
-        }}
-      >
-        {name}
-      </Text>
+      <Image source={image} style={styles.image} />
+      <Text style={styles.text}>{name}</Text>
     </View>
   );
 };
 
 export default ProjectCard;
+
+const styles = StyleSheet.create({
+  image: {
+    height: 150,
+    aspectRatio: 16 / 9,
+    borderRadius: 10,
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 20,
+    color: "dimgray",
+    marginTop: 10,
+  },
+});
